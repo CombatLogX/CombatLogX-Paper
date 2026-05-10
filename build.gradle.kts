049c43455d688b5c7370c18a9bc2b42865877ade
SirBlobman <blobmansrealm@gmail.com>
2026-05-10 13:18:34 -0400

    Initial gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+}
